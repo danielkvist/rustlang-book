61f48aea000884920ecb238677a10e7341a930bc
danielkvist <daniel@dkvist.com>
2021-02-10 13:17:47 +0100

    init guessing_game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,22 @@
+use std::io; // Imports the io library from the standard library.
+
+// Entry point of the program.
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess:");
+
+    // In Rust variables are immutable by default so
+    // we use mut before the variable name to make
+    // the variable mutable.
+    let mut guess = String::new(); // Returns a new instance of String.
+                                   // ::new() indicates that new is an associated function
+                                   // of the String type.
+
+    // Now we call the stdin() function from the io library.
+    io::stdin()
+        .read_line(&mut guess) // & indicates a reference.
+        .expect("Failed to read line"); // handles a possible error.
+
+    // Here the {} is a placeholder
+    println!("You guessed: {}", guess);
+}
