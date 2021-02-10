6e16aeb35dacca6f1a95f238881d22f730244d53
danielkvist <daniel@dkvist.com>
2021-02-10 13:06:57 +0100

    add hello_world and hello_cargo examples

---
+++ hello_world/main.rs
@@ -0,0 +1,7 @@
+// The main function is special because its always
+// the first code that runs in every Rust program.
+// It doesn't have parameters nor a return.
+fn main() {
+    // ! calls a macro
+    println!("Hello, world!");
+}
